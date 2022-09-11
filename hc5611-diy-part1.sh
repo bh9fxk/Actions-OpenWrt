bb27ed48082f15a280baff100cf091a53f246df8
Charles Cui <alavin.cui@gmail.com>
2022-09-12 00:04:15 +0800

    Update hc5611-diy-part1.sh

--- hc5611-diy-part1.sh
+++ hc5611-diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-wget 'https://github.com/BH9FXK/Actions-OpenWrt-hc5611/raw/main/hc5611.zip' --no-check-certificate && unzip -o hc5611.zip && rm -f hc5611.zip
+wget 'https://github.com/BH9FXK/Actions-OpenWrt/raw/main/hc5611.zip' --no-check-certificate && unzip -o hc5611.zip && rm -f hc5611.zip
