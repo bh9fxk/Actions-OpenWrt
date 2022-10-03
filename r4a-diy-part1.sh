5b0496604fbad9ce12938dd0eb5d6afac42d1ca2
Charles Cui <alavin.cui@gmail.com>
2022-10-03 12:57:46 +0800

    Update r4a-diy-part1.sh

--- r4a-diy-part1.sh
+++ r4a-diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 wget 'https://github.com/BH9FXK/Actions-OpenWrt/raw/main/r4a.zip' --no-check-certificate && unzip -o r4a.zip && rm -f r4a.zip
