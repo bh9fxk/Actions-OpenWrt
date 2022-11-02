9e6d27a7775dc5c9dd635ffe21e33f78f6557da1
Charles Cui <alavin.cui@gmail.com>
2022-11-02 20:33:47 +0800

    Update model.sh

--- model.sh
+++ model.sh
@@ -2,7 +2,7 @@
 #
 if [ ${{ inputs.model }} == 'hc5611' ]; then
     echo MODEL='HiWiFi HC5611' >> $GITHUB_ENV
-elseif [ ${{ inputs.model }} == 'r4a']; then
+elif [ ${{ inputs.model }} == 'r4a']; then
     echo MODEL='Xiaomi Mi Router 4A Gigabit Edition' >> $GITHUB_ENV
 else
     echo MODEL='Xiaomi Mi Router 3' >> $GITHUB_ENV
