bcdfc89f6112e2fc76d1c39fe40c7989365ebb3d
Charles Cui <alavin.cui@gmail.com>
2022-11-02 20:28:08 +0800

    Create model.sh

---
+++ model.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+if [ ${{ inputs.model }} == 'hc5611' ]; then
+    echo MODEL='HiWiFi HC5611' >> $GITHUB_ENV
+elseif [ ${{ inputs.model }} == 'r4a']; then
+    echo MODEL='Xiaomi Mi Router 4A Gigabit Edition' >> $GITHUB_ENV
+else
+    echo MODEL='Xiaomi Mi Router 3' >> $GITHUB_ENV
+fi
