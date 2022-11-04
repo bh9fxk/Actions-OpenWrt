0071dfbc4b47e90e27a1d294c0d72392c03ac90c
Charles Cui <alavin.cui@gmail.com>
2022-11-05 00:50:10 +0800

    Delete model.sh

--- model.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-if [ "${{ inputs.model }}" == "r4a" ]; then
-    echo "ROUTER_MODEL=Xiaomi Mi Router 4A Gigabit Edition" >>$GITHUB_ENV
-elif [ "${{ inputs.model }}" == "r3" ]; then
-    echo "ROUTER_MODEL=Xiaomi Mi Router 3" >>$GITHUB_ENV
-else
-    echo "ROUTER_MODEL=HiWiFi HC5611" >>$GITHUB_ENV
-fi
